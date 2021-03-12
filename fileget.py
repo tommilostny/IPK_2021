01224b79dd85fa26680770880fa9b51f8c9a2ce6
Tom Milostny <tommilostny@live.com>
2021-03-12 15:45:17 +0100

    Cleanup into separate funcs, added WHEREIS message

--- fileget.py
+++ fileget.py
@@ -1,22 +1,38 @@
 import socket, argparse, sys, re
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-n", "--nameserver", required=True)
-parser.add_argument("-f", "--surl", required=True)
-args = parser.parse_args()
+def parse_arguments():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-n", "--nameserver", required=True)
+	parser.add_argument("-f", "--surl", required=True)
+	return parser.parse_args()
 
-ip_pattern = re.compile("^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,4})$")
+def resolve_ipaddress(address: str):
+	ip_pattern = re.compile("^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,4})$")
+	if not ip_pattern.match(address):
+		print('Bad NAMESERVER format format "IP:PORT"')
+		sys.exit(1)
+	split = address.split(":")
+	ip = split[0]
+	port = int(split[1])
+	return ip, port
 
-if not ip_pattern.match(args.nameserver):
-    print('Bad NAMESERVER format format "IP:PORT"')
-    sys.exit(1)
+def resolve_surl(surl: str):
+	surl_pattern = re.compile("^(ftp://.+/.+)$");
+	if not surl_pattern.match(surl):
+		print('Bad SURL format format "ftp://SERVERNAME/FILE"')
+		sys.exit(2)
+	split = surl.split("/")
+	server = split[2]
+	path = "/".join(split[3:])
+	return server, path
 
-split = args.nameserver.split(":")
-server_ip = split[0]
-server_port = split[1]
+args = parse_arguments()
+server_ip, server_port = resolve_ipaddress(args.nameserver)
+server_name, file_path = resolve_surl(args.surl)
 
-print(server_ip)
-print(server_port)
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client_socket:
+	message = bytes(f"WHEREIS {server_name}\n", "utf-8")
+	client_socket.sendto(message, (server_ip, server_port))
+	received_msg, address = client_socket.recvfrom(2048)
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-...
+print(f"Message is: {received_msg}")
