d51bdb3b2810cbf30573f303f4a4e431f74f0eaa
Tom Milostny <tommilostny@live.com>
2021-04-14 18:17:11 +0200

    Added proj2

---
+++ ipk-l2l3-scan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ipk_l2l3_scan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
