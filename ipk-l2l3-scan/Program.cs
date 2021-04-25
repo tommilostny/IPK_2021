6dab183aed02519fb38039fab99eca11125ba4a6
tommilostny <tommilostny@live.com>
2021-04-25 20:28:21 +0200

    Downgraded to required .NET Core 3.1

--- ipk-l2l3-scan/Program.cs
+++ ipk-l2l3-scan/Program.cs
@@ -1,84 +1,92 @@
-﻿using ipk_l2l3_scan;
-using System;
+﻿using System;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Threading.Tasks;
 
-static void PrintAllInterfaces()
+namespace ipk_l2l3_scan
 {
-    Console.WriteLine("Available network interfaces:");
-    Console.ForegroundColor = ConsoleColor.Cyan;
-    Console.WriteLine("STATUS\tNAME");
-    Console.ResetColor();
-    foreach (var @interface in NetworkInterface.GetAllNetworkInterfaces())
+    class Program
     {
-        Console.Write($"{@interface.OperationalStatus}\t");
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine(@interface.Name);
-        Console.ResetColor();
-    }
-}
-
-static void ListScanningRanges(Subnet[] subnets)
-{
-    Console.WriteLine("Scanning ranges:");
-    foreach (var subnet in subnets) //list subnet ranges
-    {
-        var hostsCount = subnet.Address.AddressFamily switch
+        private static void PrintAllInterfaces()
         {
-            AddressFamily.InterNetwork => (uint)Math.Pow(2, 32 - subnet.MaskLength) - 2,
-            _ => (uint)Math.Pow(2, 128 - subnet.MaskLength) - 2
-        };
-        Console.WriteLine($"{subnet.Address}/{subnet.MaskLength} ({hostsCount} hosts)");
-    }
-}
-
-static async Task ScanNetwork(NetworkInterface @interface, int timeout, Subnet[] subnets)
-{
-    foreach (var subnet in subnets) //scan all subnets
-    {
-        Console.WriteLine();
-        try
-        {
-            var scanner = new NetworkScanner(@interface, timeout, subnet);
-            await scanner.ScanAsync();
+            Console.WriteLine("Available network interfaces:");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("STATUS\tNAME");
+            Console.ResetColor();
+            foreach (var @interface in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                Console.Write($"{@interface.OperationalStatus}\t");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(@interface.Name);
+                Console.ResetColor();
+            }
         }
-        catch
+
+        private static void ListScanningRanges(Subnet[] subnets)
         {
-            Console.Error.WriteLine($"Unable to scan subnet {subnet.Address} on interface {@interface.Name}.");
+            Console.WriteLine("Scanning ranges:");
+            foreach (var subnet in subnets) //list subnet ranges
+            {
+                var hostsCount = subnet.Address.AddressFamily switch
+                {
+                    AddressFamily.InterNetwork => (uint)Math.Pow(2, 32 - subnet.MaskLength) - 2,
+                    _ => (uint)Math.Pow(2, 128 - subnet.MaskLength) - 2
+                };
+                Console.WriteLine($"{subnet.Address}/{subnet.MaskLength} ({hostsCount} hosts)");
+            }
+        }
+
+        private static async Task ScanNetwork(NetworkInterface @interface, int timeout, Subnet[] subnets)
+        {
+            foreach (var subnet in subnets) //scan all subnets
+            {
+                Console.WriteLine();
+                try
+                {
+                    var scanner = new NetworkScanner(@interface, timeout, subnet);
+                    await scanner.ScanAsync();
+                }
+                catch
+                {
+                    Console.Error.WriteLine($"Unable to scan subnet {subnet.Address} on interface {@interface.Name}.");
+                }
+            }
+        }
+
+        public static async Task<int> Main(string[] args)
+        {
+            NetworkInterface @interface;
+            int timeout;
+            Subnet[] subnets;
+
+            try
+            {
+                (@interface, timeout, subnets) = await ArgumentParser.ParseArguments(args);
+            }
+            catch (ApplicationException) //ok exception thrown if help is being displayed
+            {
+                return 0;
+            }
+            catch (InvalidOperationException) //missing interface exception
+            {
+                Console.WriteLine($"Missing or invalid paramerer --interface.\n");
+                PrintAllInterfaces();
+                return 0;
+            }
+            catch (IndexOutOfRangeException exc) //timeout negative
+            {
+                Console.Error.WriteLine($"{exc.Message}.");
+                return 1;
+            }
+            catch //other errors printed by ArgumentParser and System.CommandLine
+            {
+                return 1;
+            }
+
+            ListScanningRanges(subnets);
+            await ScanNetwork(@interface, timeout, subnets);
+
+            return 0;
         }
     }
 }
-
-NetworkInterface @interface;
-int timeout;
-Subnet[] subnets;
-
-try
-{
-    (@interface, timeout, subnets) = await ArgumentParser.ParseArguments(args);
-}
-catch (ApplicationException) //ok exception thrown if help is being displayed
-{
-    return 0;
-}
-catch (InvalidOperationException) //missing interface exception
-{
-    Console.WriteLine($"Missing or invalid paramerer --interface.\n");
-    PrintAllInterfaces();
-    return 0;
-}
-catch (IndexOutOfRangeException exc) //timeout negative
-{
-    Console.Error.WriteLine($"{exc.Message}.");
-    return 1;
-}
-catch //other errors printed by ArgumentParser and System.CommandLine
-{
-    return 1;
-}
-
-ListScanningRanges(subnets);
-await ScanNetwork(@interface, timeout, subnets);
-
-return 0;
